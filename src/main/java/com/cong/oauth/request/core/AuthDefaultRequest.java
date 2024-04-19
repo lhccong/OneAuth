83fd56d3c1065e255a307123f75a0f82126427dc
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-19 17:07:44 +0800

    feat: 🚀新增 auth 默认请求

---
+++ src/main/java/com/cong/oauth/request/core/AuthDefaultRequest.java
@@ -0,0 +1,14 @@
+package com.cong.oauth.request.core;
+
+
+import com.cong.oauth.request.AuthRequest;
+
+/**
+ * auth 默认请求
+ *
+ * @author cong
+ * @date 2024/04/19
+ */
+public abstract class AuthDefaultRequest implements AuthRequest {
+
+}

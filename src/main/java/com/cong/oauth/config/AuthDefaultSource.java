15b0b96689dc7425e056c68465193fedea0df492
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-19 16:34:14 +0800

    feat: 🚀用枚举类分平台类型管理不同平台的配置信息

---
+++ src/main/java/com/cong/oauth/config/AuthDefaultSource.java
@@ -0,0 +1,39 @@
+package com.cong.oauth.config;
+
+
+import com.cong.oauth.request.code.AuthGithubRequest;
+import com.cong.oauth.request.core.AuthDefaultRequest;
+
+/**
+ * OneAuth 内置的各 api 需要的 url， 用枚举类分平台类型管理
+ *
+ * @author yadong.zhang (yadong.zhang0415(a)gmail.com)
+ * @since 1.0
+ */
+public enum AuthDefaultSource implements AuthSource {
+    /**
+     * Github
+     */
+    GITHUB {
+        @Override
+        public String authorize() {
+            return "https://github.com/login/oauth/authorize";
+        }
+
+        @Override
+        public String accessToken() {
+            return "https://github.com/login/oauth/access_token";
+        }
+
+        @Override
+        public String userInfo() {
+            return "https://api.github.com/user";
+        }
+
+        @Override
+        public Class<? extends AuthDefaultRequest> getTargetClass() {
+            return AuthGithubRequest.class;
+        }
+    }
+
+}

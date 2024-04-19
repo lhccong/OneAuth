13c91d2789f8f5c580aef4b71269f301f2f68d6e
ZHUT\liuhuaicong <liuhuaicong@zhut.cn>
2024-04-19 16:13:00 +0800

    feat: 🚀新增授权所需的 token

---
+++ src/main/java/com/cong/oauth/model/AuthToken.java
@@ -0,0 +1,68 @@
+package com.cong.oauth.model;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * 授权所需的token
+ *
+ * @author cong
+ * @date 2024/04/19
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthToken implements Serializable {
+    /**
+     * 默认属性
+     */
+    private String accessToken;
+    private int expireIn;
+    private String refreshToken;
+    private int refreshTokenExpireIn;
+    private String uid;
+    private String openId;
+    private String accessCode;
+    private String unionId;
+
+    /**
+     * Google附带属性
+     */
+    private String scope;
+    private String tokenType;
+    private String idToken;
+
+    /**
+     * 小米附带属性
+     */
+    private String macAlgorithm;
+    private String macKey;
+
+    /**
+     * 企业微信附带属性
+     *
+     * @since 1.10.0
+     */
+    private String code;
+    /**
+     * 微信公众号 - 网页授权的登录时可用
+     * <p>
+     * 微信针对网页授权登录，增加了一个快照页的逻辑，快照页获取到的微信用户的 uid oid 和头像昵称都是虚拟的信息
+     */
+    private boolean snapshotUser;
+
+    /**
+     * Twitter附带属性
+     *
+     * @since 1.13.0
+     */
+    private String oauthToken;
+    private String oauthTokenSecret;
+    private String userId;
+    private String screenName;
+    private Boolean oauthCallbackConfirmed;
+
+}
